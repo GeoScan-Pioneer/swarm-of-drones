22a3cdf30a8feb31ed0579fddb6b7a5d46652b04
Dgtal1 <enderdan80@gmail.com>
2021-09-17 12:29:21 +0300

    Отправка координат на сервер

--- Client1.py
+++ Client1.py
@@ -6,4 +6,7 @@ if __name__ == '__main__':
     client = Client(ip="127.0.0.1", port=8000, port_uart="COM1")
     client.run_UDP()
     client.send_message(message=client.create_message_SC(), destination=('localhost', 8000))
-    print("Sent")
+    print("Sent1")
+    time.sleep(3)
+    client.send_message(message=client.create_message_CC(10, 60, 0), destination=('localhost', 8000))
+    print("Sent2")
