70e45ae021e447406a8c019a08a6388ea6fe9bc5
Dgtal1 <enderdan80@gmail.com>
2021-09-17 18:34:54 +0300

    Подключение клиента и отправка рандомных координат

--- Client1.py
+++ Client1.py
@@ -1,6 +1,7 @@
 import time
 from SwarmUtils import Client
 from time import sleep
+from random import randint as rd
 
 if __name__ == '__main__':
     client = Client(ip="127.0.0.1", port=8000, port_uart="COM1")
@@ -8,5 +9,5 @@ if __name__ == '__main__':
     client.send_message(message=client.create_message_SC(), destination=('localhost', 8000))
     print("Sent1")
     time.sleep(3)
-    client.send_message(message=client.create_message_CC(10, 60, 0), destination=('localhost', 8000))
+    client.send_message(message=client.create_message_CC(rd(20, 580), rd(20, 580), 0), destination=('localhost', 8000))
     print("Sent2")
