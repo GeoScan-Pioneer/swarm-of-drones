045bc6bacbb9af2e7ab26c98a8f700627776d60c
Dgtal1 <enderdan80@gmail.com>
2021-09-17 11:29:19 +0300

    Вывод в консоль для проверки работы параллельных потоков

--- Server.py
+++ Server.py
@@ -3,4 +3,5 @@ from SwarmUtils import Card, Server
 if __name__ == '__main__':
     card = Card()
     server = Server(ip_server='localhost', port_serer=8000, card=card)
+    print(1)
     server.run_UDP()
