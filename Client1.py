f2ab357d00835b02d907815c78777b31248e963b
Uiopio <lesha.tort2@mail.ru>
2021-09-19 22:21:13 +0300

    Теперь на сервере сообщения принимаются в отдельном потоке (снова)
    чтобы с клиентом было больше общего. Да и можно на сервере освободить main

--- Client1.py
+++ Client1.py
@@ -2,12 +2,27 @@ import time
 from SwarmUtils import Client
 from time import sleep
 from random import randint as rd
+import random
 
 if __name__ == '__main__':
-    client = Client(ip="127.0.0.1", port=8000, port_uart="COM1")
+    client = Client(ip="127.0.0.1", port=8001, port_uart="COM1")
     client.run_UDP()
     client.send_message(message=client.create_message_SC(), destination=('localhost', 8000))
+
+
+
     print("Sent1")
     time.sleep(3)
-    client.send_message(message=client.create_message_CC(rd(20, 580), rd(20, 580), 0), destination=('localhost', 8000))
+
+    x = 50
+    y = 50
+    while True:
+        print(x, y)
+        client.send_message(message=client.create_message_CC(x, y, 0), destination=('localhost', 8000))
+        x = x + 0.1 * random.uniform(-1, 1)
+        y = y + 0.1 * random.uniform(-1, 1)
+        sleep(0.25)
+
+
+
     print("Sent2")
--- SwarmUtils.py
+++ SwarmUtils.py
@@ -6,12 +6,15 @@ import numpy as np
 import struct
 import serial
 
+import matplotlib.pyplot as plt
 import tkinter as tk
 
 from typing import List
 
 
 class Card(threading.Thread):
+    """ Класс отвечающий за карту """
+    # height, width - размеры предполагаемой карты в метрах
     def __init__(self):
         threading.Thread.__init__(self)
         self.start()
@@ -30,8 +33,9 @@ class Card(threading.Thread):
         self.canvas = tk.Canvas(self.window)
         self.canvas.pack(fill=tk.BOTH, expand=True)
 
+
         self.copter_radius = 10
-        self.copter_area_radius = 24
+        self.copter_area_radius = 30
 
         self.window.mainloop()
 
@@ -63,7 +67,10 @@ class NetUtils:
         self.port = port
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.socket.settimeout(1)
+
+        #self.socket.settimeout(1)
+        self.socket.setblocking(0)  # отключение блокировки при приеме сообщений
+
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.bind((ip, port))
@@ -127,17 +134,27 @@ class Server(NetUtils):
         # список сообщений от клиентов
         self.clients_message = []
 
-    def run_UDP(self, tasks=[]):
+    def run_UDP(self):
+
+        # Принимаем сообщения от сервера в отдельном потоке
+        stream_for_messages = threading.Thread(target=self.accepting_messages, args=())
+        stream_for_messages.daemon = True
+        stream_for_messages.start()
+
+
+
+     # В отдельном потоке принимаем сообщения
+    def accepting_messages(self):
         while True:
             try:
                 # принимаем все сообщения. После приема сообщение и клиент записываются в список
                 data, client_addr = self.socket.recvfrom(100)
                 print("Received ", data, " from ", client_addr)
-                command = self.message_handler(data, client_addr)
-                if len(tasks) > 0:
-                    tasks[0](command, data)
+                self.message_handler(data, client_addr)
             except:
                 pass
+
+
 
     ###################################
     # Блок анализа принятых сообщений #
@@ -145,7 +162,7 @@ class Server(NetUtils):
     def message_handler(self, message, client_addr):
         type_message = self.message_parser(message)
         # если пришло стартовое то запоминаем клиента
-        #if type_message.find("SC") > -1:
+
         if type_message == "SC":
             # Создаем экземпляр класса Copter
             client = Copter(num_copter=self.cid, addr=client_addr, visual=self.card.add_copter())
@@ -156,13 +173,13 @@ class Server(NetUtils):
         # Если пришли координаты
         elif type_message == "CC":
             __, X, Y, Z, __ = struct.unpack(">2sfff1c", message)
-            #client = self.get_client_by_address(client_addr)
+            client = self.get_client_by_address(client_addr)
             client = self.get_client_by_id(-1)
             self.card.canvas.moveto(client.visual[0], X+self.card.copter_radius/2, Y+self.card.copter_radius/2)
             self.card.canvas.moveto(client.visual[1], X+self.card.copter_area_radius/2, Y+self.card.copter_area_radius/2)
             print(X, Y, Z)
 
-        return type_message
+
 
     def get_client_by_address(self, addr):
         for client in self.clients:
@@ -174,6 +191,23 @@ class Server(NetUtils):
     def get_client_by_id(self, id):
         return self.clients[id]
 
+    ###################################
+    ###### Алгоритмы управления #######
+    ###################################
+
+    # вычисляет дистанцию между всеми коптерами и возвращает длину и пару коптеров, которые ближе допустимого расстояния
+    def __dist(self, p1, p2):
+        return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
+
+    def check_min_distance(self):
+        distances = []
+        for i in range(len(self.clients)):
+            for j in range(i + 1, len(self.clients)):
+                dist = self.__dist(self.clients[i].coordinates_copter[0], self.clients[j].coordinates_copter[0])
+                if dist <= 1:
+                    distances.append((i, j, dist))
+        return distances
+
 
     #########################
     # Блок тестовых функций #
@@ -182,19 +216,19 @@ class Server(NetUtils):
     def test_message(self):
         while True:
             if len(self.clients) > 0:
-                self.send_message(client=self.clients[0].addr, message=self.create_message_CA())
+                self.send_message(destination=self.clients[0].addr, message=self.create_message_CA())
                 time.sleep(2)
 
-                self.send_message(client=self.clients[0].addr, message=self.create_message_CL())
+                self.send_message(destination=self.clients[0].addr, message=self.create_message_CL())
                 time.sleep(2)
 
-                self.send_message(client=self.clients[0].addr, message=self.create_message_CD())
+                self.send_message(destination=self.clients[0].addr, message=self.create_message_CD())
                 time.sleep(2)
 
-                self.send_message(client=self.clients[0].addr, message=self.create_message_NC(10.30, 49.33, 1.00))
+                self.send_message(destination=self.clients[0].addr, message=self.create_message_NC(10.30, 49.33, 1.00))
                 time.sleep(2)
 
-                self.send_message(client=self.clients[0].addr, message=self.create_message_SL(0.30, 0.33, 1.00))
+                self.send_message(destination=self.clients[0].addr, message=self.create_message_SL(0.30, 0.33, 1.00))
 
                 time.sleep(2)
 
@@ -236,25 +270,28 @@ class Client(NetUtils):
         stream_for_messages.daemon = True
         stream_for_messages.start()
 
-        # Обрабатываем сообщения в отдельном потоке
-        stream_for_messages_handler = threading.Thread(target=self.message_handler, args=())
-        stream_for_messages_handler.daemon = True
-        stream_for_messages_handler.start()
-
-        # while True:
-        #     # принять сообщение из юарта
-        #     self.accepting_messages_uart()
-        # pass
 
     # В отдельном потоке принимаем сообщения
     def accepting_messages(self):
         while True:
+            # Принимаем сообщение из юарта
+            try:
+                self.accepting_messages_uart()
+            except:
+                #print("ошибка приема сообщения из юарта")
+                pass
+
+            # Принимаем сообщение от сервера
             try:
                 # считываем полученное сообщение
                 data, __ = self.socket.recvfrom(100)
+                self.message_handler()
                 self.server_message.append(data)  # сохраняем полученное сообщение в список
             except:
-                print("Ошибка приема сообщений от сервера")
+                #print("Ошибка приема сообщений от сервера")
+                pass
+
+
 
     # Принять сообщение из юарта
     def accepting_messages_uart(self):
