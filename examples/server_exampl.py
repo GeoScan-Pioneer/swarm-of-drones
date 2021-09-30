5c8d764227ce19c7abdca168f8f7b6f6f65afb2f
Dgtal1 <enderdan80@gmail.com>
2021-09-30 18:31:10 +0300

    Добавлено использование функции для ожидания завершения событиый коптеров

--- examples/server_exampl.py
+++ examples/server_exampl.py
@@ -12,12 +12,11 @@ if __name__ == '__main__':
     while True:
         if len(server.clients) > 0:
             if server.step == 0:
-                server.send_message(server.clients[0].addr, server.create_message_New_Coordinates(20, 20, 1))
+                server.send_message(server.clients[0].addr, server.create_message_New_Coordinates(20, 20, 1), True)
                 server.next_micro_step()
-            if server.step == 0.5:
-                if server.clients[0].task_complete_state():
-                    server.clients[0].task_complete_state_reset()
-                    server.next_micro_step()
+
+            server.wait_for_task_completion(0.5, ands=[0])
+
             if server.step == 1:
                 server.send_message(server.clients[0].addr, server.create_message_Set_Leds(255, 0, 0))
                 server.next_step()
